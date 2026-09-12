fd84a4f5500365f3078ccfbb2b2157ce3ba97e1f
Alejandro Pinto <pinto.alex05@gmail.com>
2026-09-11 22:52:13 -0400

    combustion generator uses fuel

--- crude_combustion_generator.gd
+++ crude_combustion_generator.gd
@@ -4,6 +4,19 @@ extends Area2D
 signal selected(generator)
 
 var fuel := 0
+var fuel_timer: Timer
+
+@export var fuel_consumption_time := 60
+
+
+func _ready() -> void:
+	fuel_timer = Timer.new()
+	add_child(fuel_timer)
+	
+	fuel_timer.wait_time = fuel_consumption_time
+	fuel_timer.one_shot = false
+	
+	fuel_timer.timeout.connect(fuel_tick)
 
 
 func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void:
@@ -14,4 +27,13 @@ func _input_event(viewport: Viewport, event: InputEvent, shape_idx: int) -> void
 
 
 func add_fuel(amount: int) -> void:
-	fuel += 1
+	if fuel == 0:
+		fuel_timer.start()
+		
+	fuel += amount
+
+func fuel_tick() -> void:
+	fuel -= 1
+	$"../../UI/GeneratorPanel".update_panel()
+	if fuel == 0:
+		fuel_timer.stop()
